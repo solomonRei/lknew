a2749f9782f54ede8bedc5d70f9e7fc268baff5b
Eduard <dz6class@mail.ru>
2024-03-29 12:06:15 +0200

    SSL Sertification

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use App\Models\User;
 use App\Observers\UserObserver;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -25,6 +26,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot() : void
     {
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
+
         User::observe(UserObserver::class);
     }
 }
